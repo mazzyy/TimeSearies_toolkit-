12ab38e5d197ee1beed56d9eaf21d82e27003cc1
Ali Asheer (IFAG BE DEV I C MC) <Asheer.Ali@infineon.com>
2023-08-30 23:19:37 +0200

    axios implemented

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,38 @@
+# to run the file  uvicorn main:app --reload
+import pandas as pd
+import numpy as np
+from dataProcessor.DataProcessor import DataProcessor
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
+import json
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3000/excel",
+    "http://127.0.0.1:3000/excel",
+    "http://127.0.0.1:3000/",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/test2")
+async def read_data1(request: Request):
+    data = await request.json()
+    # print(data)
+    return "success"
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"world is": "blind"}
--- frontend/src/views/Tskit.js
+++ frontend/src/views/Tskit.js
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
-import 'assets/css/index.css';
 import Papa from 'papaparse';
 import {
-  Badge,
-  Button,
-  Card,
-  Form,
-  Navbar,
-  Nav,
   Container,
   Row,
-  Col
+  Col,
+  Card,
+  Form,
 } from "react-bootstrap";
+import axios from "axios";
+import { Button } from "antd";
 
-function Tskit() {
-
+const Tskit = () => {
   const [csvData, setCsvData] = useState(null); // State to store parsed CSV data
 
   const handleFileChange = (e) => {
@@ -30,6 +26,25 @@ function Tskit() {
       });
     }
   };
+const dummy = {data: [[1,2,3],[4,5,6]]}
+  const handleSubmit = () => {
+    const url = "http://127.0.0.1:8000/test2";
+    axios
+      .post(url, {
+        data: csvData,
+        headers: { "Access-Control-Allow-Origin": "*" },
+      })
+      .then((response) => {
+        // Handle the response as needed
+        console.log("Response:", response);
+
+        // Update the state to hide the submit link and show the new link
+      })
+      .catch((error) => {
+        // Handle errors if any
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <>
@@ -44,43 +59,41 @@ function Tskit() {
                 <Form>
                   <Row>
                     <Col className="pr-1 " md="12">
-                    <Form className="my-5">
-                  <Form.Group>
-                    <Form.Label>Name for project</Form.Label>
-                    <Form.Control
-                      placeholder="Name"
-                      type="text"
-                      value='123'
-                      // onChange={handleNameChange}
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Upload File</Form.Label>
-                    <Form.Control
-                      type="file"
-                      className="custom-file-upload "
-                      onChange={handleFileChange} // Handle file change
-
-                    />
-                  </Form.Group>
-                  <Button
-                    className="btn-fill pull-right mt-5"
-                    type="submit"
-                    variant="info"
-                    onClick={(e) => e.preventDefault()} 
-                  >
-                  Save and upload
-                  </Button>
-
-                </Form>
+                      <Form className="my-5">
+                        <Form.Group>
+                          <Form.Label>Name for project</Form.Label>
+                          <Form.Control
+                            placeholder="Name"
+                            type="text"
+                            value='123'
+                            // onChange={handleNameChange}
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Label>Upload File</Form.Label>
+                          <Form.Control
+                            type="file"
+                            className="custom-file-upload "
+                            onChange={handleFileChange} // Handle file change
+                          />
+                        </Form.Group>
+                        <>default train test is 70:30</>
+                        <br></br>
+                        <div> select model lstm or Prophet</div>
+                        <br></br>
+                        <Button
+                          className="btn-fill pull-right mt-5"
+                          type="submit"
+                          variant="info"
+                          onClick={handleSubmit}
+                        >
+                          Save and upload
+                        </Button>
+                      </Form>
                     </Col>
-
-
                   </Row>
                   <Row>
-
                   </Row>
-
                   <div className="clearfix"></div>
                 </Form>
               </Card.Body>
@@ -98,24 +111,19 @@ function Tskit() {
                 <div className="author">
                   <a href="#pablo" onClick={(e) => e.preventDefault()} >
                     <span
-
                       className="avatar border-gray"
-
                     ></span>
                     <br/><br/><br/><br/><br/>
                     <h5 className="title">Tips</h5>
                   </a>
-
                 </div>
                 <p className="description text-center">
-                The data should consists of columns/features with time, and it should only have two columns: 'x' representing time and 'y' representing prediction values.
+                The data should consist of columns/features with time, and it should only have two columns: 'x' representing time and 'y' representing prediction values.
                 </p>
               </Card.Body>
               {/* <>This is the data</>
               <div> {JSON.stringify(csvData)}</div> */}
               <hr></hr>
-
-
             </Card>
           </Col>
         </Row>
